52d71f45147990254be6875ab2ecb25c2d5da805
Jacob Bryant <jacobryas4@gmail.com>
2019-11-21 16:50:14 -0500

    added authentication files, left a file out to protect firebase api key

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import classes from './Auth.css'
+import * as actions from '../../store/actions/index'
 
 class Auth extends Component {
 
@@ -70,6 +72,24 @@ class Auth extends Component {
         return isValid
     }
 
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        }
+        this.setState({controls: updatedControls})
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    }
+
     render() {
 
         const formElementsArray = [];
@@ -95,7 +115,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">Submit</Button>
                 </form>
@@ -104,4 +124,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
